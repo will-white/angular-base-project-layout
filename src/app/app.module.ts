268e915a42bf6e398323f6c1b4ef1ae2b3d39fd8
will-white <github@willwhite.dev>
2020-03-20 14:23:48 -0500

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { NavBarComponent } from "./core/nav-bar/nav-bar.component";
-import { FooterComponent } from "./core/footer/footer.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 import { SearchBoxComponent } from "./core/search/search-box/search-box.component";
 import { SearchResultsComponent } from "./core/search/search-results/search-results.component";
 import { ReportingErrorHandler } from "./core/reporting-error-handler";
