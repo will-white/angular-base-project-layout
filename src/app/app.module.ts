25d5969c5a1082e47e39b9a7d8d578c2e1c59ee2
will-white <github@willwhite.dev>
2020-03-20 13:24:49 -0500

    2 files modified, 15 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { ErrorHandler, BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { NavBarComponent } from "./core/nav-bar/nav-bar.component";
+import { FooterComponent } from "./core/footer/footer.component";
+import { SearchBoxComponent } from "./core/search/search-box/search-box.component";
+import { SearchService } from "./core/search/search.service";
+import { SearchResultsComponent } from "./core/search/search-results/search-results.component";
+import { ReportingErrorHandler } from "./core/reporting-error-handler";
+import { WindowToken, windowProvider } from "./core/window";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatSidenavModule,
+    MatToolbarModule
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NavBarComponent,
+    FooterComponent,
+    SearchBoxComponent,
+    SearchResultsComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: ErrorHandler, useClass: ReportingErrorHandler },
+    { provide: WindowToken, useFactory: windowProvider }
+  ]
 })
-export class AppModule { }
+export class AppModule {}
